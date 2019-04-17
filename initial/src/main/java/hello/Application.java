e31a4144df1ac76bb996b69c9145dcde2b85bb1c
harshblog150 <harshblog150@gmail.com>
2019-04-17 15:30:17 +0530

    commit 1

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -11,10 +11,10 @@ public class Application {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        //---
+        //-------
         //ctx.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
         //ctx.run(args);
-        //---
+        //-------
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         
         String[] beanNames = ctx.getBeanDefinitionNames();
